b09af122f2ef9d923118fe8f4b9baefbea0ccadc
Yam Khadka <165254553+Yam556@users.noreply.github.com>
2024-11-18 20:52:17 +1100

    save change

---
+++ Javascript_code/app.js
@@ -0,0 +1,10 @@
+/*
+Author: Yam Khadka
+    email: yamk6385@gmail.com
+    version:1
+    Description: This the the fiorst javascript file written by me.
+*/
+
+let normalName = 'Alex';
+let javascriptName = normalName + 'script';
+console.log('Your JavaScript Name is ' + javascriptName);
